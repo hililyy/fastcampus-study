afa05ec279b927f7161f714bca008fe7c84372cf
hililyy <joun406@gmail.com>
2021-08-26 02:04:38 +0900

    스탑워치 수정

---
+++ Clock/StioWatchTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StioWatchTableViewCell.swift
+//  Clock
+//
+//  Created by 강조은 on 2021/08/24.
+//
+
+import UIKit
+
+class MainTableviewCell: UITableViewCell {
+    static let cell_id = "cell_id_lap"
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+}
+
--- Clock/StopWatchViewController.swift
+++ Clock/StopWatchViewController.swift
@@ -15,6 +15,8 @@ class StopWatchViewController: UIViewController {
     var mainTimer:Timer?
     var isStart = true
     var isStop = false
+    var tableData = [String]()
+    var count = 1
     
     @IBOutlet var tableView: UITableView!
     @IBOutlet var btnRight: UIButton!
@@ -28,12 +30,14 @@ class StopWatchViewController: UIViewController {
         btnLeft.isEnabled = false
         btnRight.isEnabled = true
         
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
         
     }
     
     func startAction() {
-        mainTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (_) in
+        mainTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (_) in
                     self.timeCount += 1
                     DispatchQueue.main.async {
                        let timeString = self.makeTimeLabel(count: self.timeCount)
@@ -42,11 +46,12 @@ class StopWatchViewController: UIViewController {
                     }
             })
     }
+    
     func makeTimeLabel(count:Int) -> (String,String) {
         //return - (TimeLabel, decimalLabel)
-        let decimalSec = count % 10
-        let sec = (count / 10) % 60
-        let min = (count / 10) / 60
+        let decimalSec = count % 100 // 나누기 100으로 하는게 맞는듯..
+        let sec = (count / 100) % 60
+        let min = (count / 100) / 60
         let sec_string = "\(sec)".count == 1 ? "0\(sec)" : "\(sec)"
         let min_string = "\(min)".count == 1 ? "0\(min)" : "\(min)"
         return ("\(min_string):\(sec_string)","\(decimalSec)")
@@ -57,7 +62,7 @@ class StopWatchViewController: UIViewController {
         mainTimer = nil
     }
     
-    @IBAction func RightTime(_ sender: Any) {
+    @IBAction func RightTime(_ sender: UIButton) {
         // 초기 상태에서 시작을 눌렀을 때
         if(isStart){
             startAction()
@@ -65,6 +70,7 @@ class StopWatchViewController: UIViewController {
             btnLeft.setTitle("랩", for: .normal)
             isStop = true
             isStart = false
+            // 중단을 할 수 있는 상태, 시작을 못 하는 상태 (시작을 눌렀기 때문)
             btnLeft.isEnabled = true
         }
         // 중단을 눌렀을때
@@ -76,8 +82,9 @@ class StopWatchViewController: UIViewController {
             isStart = true
         }
     }
-    // 재설정을 눌렀을 때
+   
     @IBAction func LeftTime(_ sender: UIButton) {
+        // 재설정을 눌렀을 때
         if(isStart){
             btnRight.setTitle("시작", for: .normal)
             btnLeft.setTitle("랩", for: .normal)
@@ -85,13 +92,42 @@ class StopWatchViewController: UIViewController {
             timeCount = 0
             lblTime.text = "00:00"
             lblDecimal.text = ".00"
+            self.tableData.removeAll()
+            self.tableView.reloadData()
+            count = 1
+        }
+        // 랩을 눌렀을 때
+        else if(!isStart) {
             
+            let timeString = self.makeTimeLabel(count: self.timeCount)
+            self.tableData.append("\(timeString.0).\(timeString.1)")
+            let indexPath = IndexPath(row: self.tableData.count - 1, section: 0)
+            
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+          //  self.tableView.scrollToRow(at: indexPath, at: .none, animated: true)
+            
+
         }
         
     }
     
+}
+
+extension StopWatchViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData.count
+    }
     
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: StopWatchTableViewCell.cell_id, for: indexPath) as! StopWatchTableViewCell
+        
+        let item = self.tableData[indexPath.row]
+            cell.lblTime.text = item
+            cell.lblLabIndex.text = "랩 " + String(count)
+            count = count + 1
+            return cell
+
+    }
 }
     
 
