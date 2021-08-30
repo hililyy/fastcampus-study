aabac7bd451af4e9c2dd88010bba253fc20fda0b
hililyy <joun406@gmail.com>
2021-08-30 18:30:54 +0900

    210830

--- Clock/GlobalClockCityListTableViewController.swift
+++ Clock/GlobalClockCityListTableViewController.swift
@@ -30,7 +30,8 @@ class GlobalClockCityListTableViewController: UITableViewController {
     
     private let sections: [String] = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ","ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
     
-
+//    cityListView.delegate = self
+//    cityListView.dataSource = self
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -52,40 +53,81 @@ class GlobalClockCityListTableViewController: UITableViewController {
     // 섹션 내부 셀의 각각의 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        switch section
-        {
-        case 0 : return cityItem1.count
-        case 1 : return cityItem2.count
-        case 2 : return cityItem3.count
-        case 3 : return cityItem4.count
-        case 4 : return cityItem5.count
-        case 5 : return cityItem6.count
-        case 6 : return cityItem7.count
-        case 7 : return cityItem8.count
-        case 8 : return cityItem9.count
-        case 9 : return cityItem10.count
-        case 10 : return cityItem11.count
-        case 11 : return cityItem12.count
-        case 12 : return cityItem13.count
-        case 13 : return cityItem14.count
-        default:
-            return 0
-        }
+//        switch section
+//        {
+//        case 0 : return cityItem1.count
+//        case 1 : return cityItem2.count
+//        case 2 : return cityItem3.count
+//        case 3 : return cityItem4.count
+//        case 4 : return cityItem5.count
+//        case 5 : return cityItem6.count
+//        case 6 : return cityItem7.count
+//        case 7 : return cityItem8.count
+//        case 8 : return cityItem9.count
+//        case 9 : return cityItem10.count
+//        case 10 : return cityItem11.count
+//        case 11 : return cityItem12.count
+//        case 12 : return cityItem13.count
+//        case 13 : return cityItem14.count
+//        default:
+//            return 0
+//        }
+        return 2
     }
    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
             return 30
         }
-    /*
+    
+    override func tableView(_ tableView:UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell", for: indexPath)
         
-        cell.textLabel?.text = cityItem1[(indexPath as NSIndexPath).row]
+        if indexPath.section == 0 {
+            cell.textLabel?.text = cityItem1[indexPath.row] }
+        else if indexPath.section == 1 {
+            cell.textLabel?.text = "\(cityItem2[indexPath.row])" }
+        else if indexPath.section == 2 {
+            cell.textLabel?.text = "\(cityItem3[indexPath.row])" }
+        else if indexPath.section == 3 {
+            cell.textLabel?.text = "\(cityItem4[indexPath.row])" }
+        else if indexPath.section == 4 {
+            cell.textLabel?.text = "\(cityItem5[indexPath.row])" }
+        else if indexPath.section == 5 {
+            cell.textLabel?.text = "\(cityItem6[indexPath.row])" }
+        else if indexPath.section == 6 {
+            cell.textLabel?.text = "\(cityItem7[indexPath.row])" }
+        else if indexPath.section == 7 {
+            cell.textLabel?.text = "\(cityItem8[indexPath.row])" }
+        else if indexPath.section == 8 {
+            cell.textLabel?.text = "\(cityItem9[indexPath.row])" }
+        else if indexPath.section == 9 {
+            cell.textLabel?.text = "\(cityItem10[indexPath.row])" }
+        else if indexPath.section == 10 {
+            cell.textLabel?.text = "\(cityItem11[indexPath.row])" }
+        else if indexPath.section == 11 {
+            cell.textLabel?.text = "\(cityItem12[indexPath.row])" }
+        else if indexPath.section == 12 {
+            cell.textLabel?.text = "\(cityItem13[indexPath.row])" }
+        else if indexPath.section == 13 {
+            cell.textLabel?.text = "\(cityItem14[indexPath.row])" }
+        
+        else {
+            return UITableViewCell()
+            
+        }
+
+        
         // Configure the cell...
 
         return cell
     }
     
-    */
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
