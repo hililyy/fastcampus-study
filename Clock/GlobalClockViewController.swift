32fd1b1a653892f4c1f66cabcf89617a485d104d
hililyy <joun406@gmail.com>
2021-09-02 22:32:55 +0900

    210902

--- Clock/GlobalClockViewController.swift
+++ Clock/GlobalClockViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class GlobalClockViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    var city : [String] = ["서울","가보로네"]
+    
     @IBOutlet var cityListView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,26 +17,48 @@ class GlobalClockViewController: UIViewController, UITableViewDelegate, UITableV
         cityListView.dataSource = self
         // Do any additional setup after loading the view.
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return city.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "selectCityCell", for: IndexPath) as! selectCityCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "selectCityCell", for: indexPath) as! selectCityCell
         
-        cell.lblCityClock = ""
+        //임시 데이터
+        cell.lblCityClock.text = "서울"
+        cell.lblCityClock.text = "00:00"
+        cell.lblNowClock1.text = "오늘,"
+        cell.lblNowClock2.text = "+0시간"
         return cell
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 93;
     }
-    */
-
+    
+    
+    @IBAction func editButton(_ sender: UIBarButtonItem) {
+//        if self.cityListView.isEditing {
+//            self.editButton.s
+//        }
+//        cityListView.edit(with: sender, to: UITableView)
+//
+        
+        
+    }
+    
+    func edit(with button: UIBarButtonItem, to tableView: UITableView) {
+            if tableView.isEditing {
+                button.title = "Edit"
+                tableView.setEditing(false, animated: true)
+            } else {
+                button.title = "Done"
+                tableView.setEditing(true, animated: true)
+            }
+    }
 }
