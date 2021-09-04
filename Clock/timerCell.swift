2fd9973f186e895e749b4298fe553d3d677b48e6
hililyy <joun406@gmail.com>
2021-09-05 01:37:26 +0900

    210904

---
+++ Clock/timerCell.swift
@@ -0,0 +1,23 @@
+//
+//  timerCell.swift
+//  Clock
+//
+//  Created by 강조은 on 2021/09/04.
+//
+
+import UIKit
+
+class timerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
