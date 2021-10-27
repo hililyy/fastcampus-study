38ec237205cdd571b9935acbf5cc62c2820b2eed
hililyy <joun406@gmail.com>
2021-10-27 14:23:49 +0900

    LED 전광판 앱

---
+++ ScreenTransitionExample/ScreenTransitionExample/SeguePushViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SeguePushViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by 강조은 on 2021/10/25.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
---
+++ ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by 강조은 on 2021/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
