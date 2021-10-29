5a17ef317fbde8a24e5c84fe5f9b18223a305953
hililyy <joun406@gmail.com>
2021-10-29 23:38:50 +0900

    LED 전광판 앱

---
+++ LEDBoard/LEDBoard/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by 강조은 on 2021/10/29.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
